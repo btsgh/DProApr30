c815b7ae4e6e48ce75920ae89a5b3db974c498c3
Aparna Ramachandran <aparna@appsmith.com>
2025-04-30 04:43:12 +0000

    System generated commit, initial commit

---
+++ pages/Inbuilt Framework/jsobjects/JSObject2InBuiltLib/JSObject2InBuiltLib.js
@@ -0,0 +1,19 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		var a = _.add(3525,3525)
+		return a
+		
+		console.log (_.add (3525,5325))
+		console.log (moment.months())
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	myFun2 () {
+		var b = moment.months()
+		return b
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
